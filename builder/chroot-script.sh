ee71926174e52a0d58cb9cc1c720fc7f9a24adf9
ANCELIN Julien <julien.ancelin@inra.fr>
2018-11-28 16:33:35 +0100

    update geopoppy install with cartman

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -264,12 +264,55 @@ wget --no-check-certificate -P /etc https://raw.githubusercontent.com/jancelin/r
 wget --no-check-certificate -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/rc.local 
 chmod +x  /etc/rc.local 
 
-# create startup script
+##get check_docker.sh
+wget --no-check-certificate -P /home/pirate https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/check_docker.sh 
+wget --no-check-certificate -P /etc/systemd/system https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/Cdocker.service 
+
+#get docker-compose.yml
+wget --no-check-certificate -P /home/pirate https://raw.githubusercontent.com/jancelin/geo-poppy/master/docker-compose-flash.yml
+mv /home/pirate/docker-compose-flash.yml /home/pirate/docker-compose.yml
+
+#get postgresql backup sql
+wget --no-check-certificate -P /home/pirate https://raw.githubusercontent.com/jancelin/docker-postgis/master/setup-database.sh
+wget --no-check-certificate -P /home/pirate https://github.com/jancelin/docker-postgis/raw/master/geopoppy.sql
+
+# import docker images
 mkdir /src && cd /src
+wget https://cartman.sig.inra.fr/geopoppy/docker/portainer.tar.gz
+wget https://cartman.sig.inra.fr/geopoppy/docker/postgres10-2.4-arm32_1.tar.gz
+wget https://cartman.sig.inra.fr/geopoppy/docker/qgis3server.tar.gz
+wget https://cartman.sig.inra.fr/geopoppy/docker/lizmap_rpi3_3.2rc6.tar.gz
+wget https://cartman.sig.inra.fr/geopoppy/docker/rpi-cloudcmd.tar.gz
+wget https://cartman.sig.inra.fr/geopoppy/docker/redis4.tar.gz
+wget https://cartman.sig.inra.fr/geopoppy/docker/tracking_1_0.tar.gz
+
+# create startup script
 cat << EOF > /src/start.sh
 #!/bin/bash
+cd /src
+docker load --input portainer.tar.gz
+docker load --input postgres10-2.4-arm32_1.tar.gz
+docker load --input qgis3server.tar.gz
+docker load --input lizmap_rpi3_3.2rc6.tar.gz
+docker load --input rpi-cloudcmd.tar.gz
+docker load --input redis4.tar.gz
+docker load --input tracking_1_0.tar.gz
+cd /home/pirate
+docker-compose up -d &&
+# systemctl checkdocker.sh
+chmod +x /home/pirate/check_docker.sh
+systemctl enable Cdocker.service
+systemctl start Cdocker.service
+#modif du compose: https://github.com/jancelin/geo-poppy/blob/master/install/auto_install_geopoppy_32bits.sh
+fLine=$(awk '/entrypoint:/ { print NR}' docker-compose.yml)&&
+endLine=$((fLine+2))&&
+for i in `seq $fLine  $endLine `
+do
+sed -i -e "$i s/^ /#/" docker-compose.yml
+done
 #blink green led for display Victory!!
-for i in `seq 1 24`; do
+for i in `seq 1 24`
+do
 sudo sh -c "echo default-on > /sys/class/leds/led0/trigger"   # Static LED on
 sleep 0.2
 sudo sh -c "echo none > /sys/class/leds/led0/trigger"        # Static LED off
@@ -287,9 +330,7 @@ sleep 1.5
 sudo sh -c "echo none > /sys/class/leds/led0/trigger"        # Static LED off
 sleep 0.2
 done
-
 sudo sh -c "echo mmc0 > /sys/class/leds/led0/trigger"        # The normal behaviour
-
 EOF
 
 chmod 770 /src/start.sh
